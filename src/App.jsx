d5d00263a039bea23d4fbc06e305d3b108a6b46c
yvonnebongers <yvonnebongers@gmail.com>
2025-12-17 20:46:03 +0100

    begin opdracht 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css';
-
+import axios from 'axios';
 function App() {
+async function countriesData(){
+    try {
+    const result = await
+        axios.get(`https://restcountries.com/v3.1/all?fields=name,flags,population`)
+console.log(result.data);
+}catch (error)
+    {
+        console.error(error);
+    }
+}
 
+countriesData();
     return (
         <>
-            Maak je applicatie hier!
+        <button >Informatie over landen</button>
         </>
     )
 }
